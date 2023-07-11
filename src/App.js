d7d455f6b1fa300a8daa1ce3d79df7bd1678ced8
Swastik Jha <mr.swastikjha@gmail.com>
2023-07-11 18:35:24 +0530

    App modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { Provider } from "react-redux";
 import TodoForm from "./components/ToDoForm/ToDoForm";
 import TodoList from "./components/ToDoList/ToDoList";
+import store from "./redux/store";
 
 import './App.css';
 
@@ -20,9 +22,10 @@ function App() {
   return (
     <div>
       <h1>To Do App</h1>
-
-      <TodoForm onCreateTodo={createTodo} />
-      <TodoList todos={todos} onToggle={toggleTodo} />
+      <Provider store={store}>
+        <TodoForm onCreateTodo={createTodo} />
+        <TodoList todos={todos} onToggle={toggleTodo} />
+      </Provider>
     </div>
   );
 }
